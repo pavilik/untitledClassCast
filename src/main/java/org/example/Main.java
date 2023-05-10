85b0682e49a13ed099b8cfea736136be4a0946af
Pavel Yakushik <ext.pyakushik@tinkoff.ru>
2023-05-10 13:54:58 +0300

    задача №1 (https://coreapp.ai/app/player/lesson/62dacf698f43b29ceb4f4523/1/62dacf698f43b29ceb4f4526)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,61 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File csvFile = new File("src/main/resources/Задача ВС Java Сбер.csv");
+
+        Scanner in = new Scanner(csvFile);
+        in.useDelimiter("\n");
+        List<City> cities = new ArrayList<>();
+
+        while (in.hasNext()) {
+            String[] splitedNext = in.next().split(";");
+            cities.add(
+                    new City(
+                            splitedNext[1],
+                            splitedNext[2],
+                            splitedNext[3],
+                            Integer.parseInt(splitedNext[4]),
+                            splitedNext[5].trim()
+                    )
+            );
+        }
+        System.out.println(cities);
+
+    }
+
+    public static class City {
+        private final String name;// наименование города
+        private final String region; // регион
+        private final String district; // федеральный округ
+        private final int population; // количество жителей города
+        private final String foundation; // дата основания или первое упоминан
+
+        public City(String name, String region, String district, int population, String foundation) {
+            this.name = name;
+            this.region = region;
+            this.district = district;
+            this.population = population;
+            this.foundation = foundation;
+        }
 
 
+        @Override
+        public String toString() {
+            return "City{" +
+                    "name='" + name + '\'' +
+                    ", region='" + region + '\'' +
+                    ", district='" + district + '\'' +
+                    ", population=" + population +
+                    ", foundation='" + foundation + '\'' +
+                    '}'+"\n\n";
+        }
     }
 }
